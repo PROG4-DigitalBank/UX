47dfa9259e12dc9a9010266b066f9904ca3c5838
Aina Dylan Rasamoelina <hei.dylan.3@gmail.com>
2024-03-17 10:48:16 +0300

    feat: CustomerController implemented

---
+++ src/main/java/com/cosmetica/bank/controller/CustomerController.java
@@ -0,0 +1,39 @@
+// CustomerController.java
+package com.cosmetica.bank.controller;
+
+import com.cosmetica.bank.model.Customer;
+import com.cosmetica.bank.service.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<Customer> signUp(@RequestParam String firstName, @RequestParam String lastName,
+                                           @RequestParam String email, @RequestParam String password,
+                                           @RequestParam String phoneNumber, @RequestParam BigDecimal monthlySalary) {
+        Customer customer = customerService.signUp(firstName, lastName, email, password, phoneNumber, monthlySalary);
+        return new ResponseEntity<>(customer, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Customer> login(@RequestParam String email, @RequestParam String password) {
+        try {
+            Customer customer = customerService.login(email, password);
+            return ResponseEntity.ok(customer);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+}
---
+++ src/main/java/com/cosmetica/bank/repository/DatabaseConnection.java
@@ -0,0 +1,29 @@
+package com.cosmetica.bank.repository;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConnection {
+
+    private static final Properties properties = new Properties();
+
+    static {
+        try {
+            properties.load(new FileInputStream("config.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static final String URL = properties.getProperty("db.url");
+    private static final String USERNAME = properties.getProperty("db.username");
+    private static final String PASSWORD = properties.getProperty("db.password");
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+}
