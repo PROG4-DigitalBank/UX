f9f3d9da15054d6a76ec5254a1b3261a13e71442
Ramilison Michel Heather <hei.michel.8@gmail.com>
2024-03-19 14:36:40 +0300

    fix: date constraint function fixed

--- src/main/resources/db/migration/V2__Alter_Table.sql
+++ src/main/resources/db/migration/V2__Alter_Table.sql
@@ -1,2 +1,2 @@
 -- Add the new constraint to check date of birth is at least 21 years ago
-ALTER TABLE customers ADD CONSTRAINT check_date_of_birth CHECK (date_of_birth <= DATE_SUB(CURRENT_DATE, INTERVAL '21 years'));
+ALTER TABLE customers ADD CONSTRAINT check_date_of_birth CHECK (date_of_birth <= CURRENT_DATE - INTERVAL '21 years');
