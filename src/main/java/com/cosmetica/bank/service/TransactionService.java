dc58242bf3eb9dfc8dd2604a7ada23e047455f00
Ramilison Michel Heather <hei.michel.8@gmail.com>
2024-03-24 16:54:33 +0300

    feat: Add planning and cancellation features in TransactionService

--- src/main/java/com/cosmetica/bank/service/TransactionService.java
+++ src/main/java/com/cosmetica/bank/service/TransactionService.java
@@ -4,6 +4,7 @@ package com.cosmetica.bank.service;
 import com.cosmetica.bank.model.Transaction;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TransactionService {
@@ -12,6 +13,9 @@ public interface TransactionService {
     String withdraw(Long accountId, BigDecimal amount);
 
     String transfer(Long sourceAccountId, Long targetAccountId, BigDecimal amount);
+    String scheduleTransfer(Long sourceAccountId, Long targetAccountId, BigDecimal amount, LocalDateTime effectiveDateTime);
+
+    String cancelScheduledTransfer(Long transactionId);
 
     BigDecimal calculateLoansAmount(Long accountId);
 
